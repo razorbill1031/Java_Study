f1a46de4ec499346d144e062eaffe21286270a16
razorbill1031 <razorbill1031@gmail.com>
2026-04-16 21:32:59 +0900

    Day 7

---
+++ src/classpart/Student.java
@@ -0,0 +1,18 @@
+package classpart;
+
+public class Student {
+    int studentID;
+    String studentName;
+    int grade;
+    String address;
+
+    /* public void showStudentInfo( ) {
+        System.out.println(studentName + "," + address); */
+
+    /* public String getStudentName( ) {
+        return  studentName; 학생 이름을 반환하는 메서드 구현*/
+
+    /* public void setStudentName(String name) {
+        studentName = name; 학생 이름을 부여하는 메서드 구현*/
+    }
+}
