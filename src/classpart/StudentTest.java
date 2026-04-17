feac5d4ad686c559799e5770b26bbae86a25d2ba
razorbill1031 <razorbill1031@gmail.com>
2026-04-17 18:20:35 +0900

    Day 8

--- src/classpart/StudentTest.java
+++ src/classpart/StudentTest.java
@@ -1,18 +1,9 @@
 package classpart;
 
-public class Student {
-    int studentID;
-    String studentName;
-    int grade;
-    String address;
-
-    public String getStudentName( ) {
-        return  studentName;
-    }
-
+public class StudentTest {
     public static void main(String[] args) {
         Student studentAhn = new Student( ); //Student 클래스 생성
-        studentAhn.studentName = "안연수";
+        studentAhn.studentName = "안승연";
 
         System.out.println(studentAhn.studentName);
         System.out.println(studentAhn.getStudentName( ));
