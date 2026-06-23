39705f189bd43efd49050d32bb69a96eb291573b
razorbill1031 <razorbill1031@gmail.com>
2026-06-23 22:05:53 +0900

    Day 26

--- src/collection/Member2.java
+++ src/collection/Member2.java
@@ -1,12 +1,12 @@
 package collection;
 
-import singleton.Company;
+import java.util.Comparator;
 
-public class Member implements Comparable<Member> {
+public class Member2 implements Comparator<Member2> {
     private int memberId;
     private String memberName;
 
-    public Member(int memberId, String memberName) {
+    public Member2(int memberId, String memberName) {
         this.memberId = memberId;
         this.memberName = memberName;
     }
@@ -39,8 +39,8 @@ public class Member implements Comparable<Member> {
 
     @Override
     public boolean equals(Object obj) {
-        if (obj instanceof Member) {
-            Member member = (Member)obj;
+        if (obj instanceof Member2) {
+            Member2 member = (Member2)obj;
             if (this.memberId == member.memberId)
                 return true;
             else
@@ -50,7 +50,7 @@ public class Member implements Comparable<Member> {
     }
 
     @Override
-    public int compareTo(Member member) {
-        return(this.memberId-member.memberId);
+    public int compare(Member2 mem1, Member2 mem2){
+        return mem1.getMemberId() - mem2.getMemberId();
     }
 }
